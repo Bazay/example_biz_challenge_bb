119c352785fbd6e7347484e0f0412ac6543b4b10
Baron <baronbloomer@gmail.com>
2015-07-07 15:23:22 +0100

    Order model tests skeleton

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validates" do
+    it "order_date format"  
+    it "order_date presence"
+    it "customer_id is an integer"
+    it "customer_id presence"
+    it "supplier_id is an integer"
+    it "supplier_id presence"
+    it "delivery_address presence"
+    it "total_order_value is a float"
+    it "total_order_value presence"
+    it "currency format"
+    it "currency presence"
+  end
 end
